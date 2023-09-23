0268d26e312b82fdf217aac17e0f4db6d520b58f
3p0d_on_off <3p0d.on.off@gmail.com>
2023-09-23 13:14:36 +0200

    [dev]rework onready variables

--- 3p0d_lowPolyCharacter_workshop/_DEV/Scripts/spawnCharacter.gd
+++ 3p0d_lowPolyCharacter_workshop/_DEV/Scripts/spawnCharacter.gd
@@ -1,27 +1,30 @@
 extends Node3D
 
 #ONREADY VARIABLES:
+@onready var _characterName : Label = $"../_UI/Control_Headers/Control_LeftMenu/VBox_texts/Label_CharacterName"
+@onready var _artistName : Label = $"../_UI/Control_Headers/Control_LeftMenu/VBox_texts/HBox_MadeByArtistName/Label_ArtistName"
+@onready var _characterDescription : Label = $"../_UI/Control_Headers/Control_LeftMenu/VBox_texts/Label_Description"
+@onready var _madeby : Label = $"../_UI/Control_Headers/Control_LeftMenu/VBox_texts/HBox_MadeByArtistName/Label_MadeBy"
+@onready var _spt01text : Label = $"../_UI/Control_Headers/Control_RightMenu/VBox_sprites/HBox_sprite1/VBox_sprite1/Label_sprite1text"
+@onready var _spt01title : Label = $"../_UI/Control_Headers/Control_RightMenu/VBox_sprites/HBox_sprite1/VBox_sprite1/Label_sprite1title"
+@onready var _spt02text : Label = $"../_UI/Control_Headers/Control_RightMenu/VBox_sprites/HBox_sprite2/VBox_sprite2/Label_sprite2text"
+@onready var _spt02title : Label = $"../_UI/Control_Headers/Control_RightMenu/VBox_sprites/HBox_sprite2/VBox_sprite2/Label_sprite2title"
+@onready var _spt01 : TextureRect = $"../_UI/Control_Headers/Control_RightMenu/VBox_sprites/HBox_sprite1/TextureRect_sprite1"
+@onready var _spt02 : TextureRect = $"../_UI/Control_Headers/Control_RightMenu/VBox_sprites/HBox_sprite2/TextureRect_sprite2"
+@onready var _btnArrowL : Button = $"../_UI/Control_Arrows/Button_arrowLeft"
+@onready var _btnArrowR : Button = $"../_UI/Control_Arrows/Button_arrowRight"
+@onready var _btnMenu : Button = $"../_UI/Control_Menus/MarginContainer_Menu/Button_Menu"
+@onready var _btnQuit : Button = $"../_UI/Control_Menus/MarginContainer_Quit/Button_Quit"
+@onready var _stgpanel : Panel = $"../_UI/Control_Settings/Panel_Settings"
 
 #PUBLIC VARIABLES:
 @export var m_rotSpd = 3
 @export var m_characters:Array[PackedScene]
-@export var m_characterName : Label
-@export var m_artistName : Label
-@export var m_characterDescription : Label
-@export var m_madeby : Label
-@export var m_spt01text : Label
-@export var m_spt01title : Label
-@export var m_spt02text : Label
-@export var m_spt02title : Label
-@export var m_spt01 : TextureRect
-@export var m_spt02 : TextureRect
-@export var m_btnArrowL : Button
-@export var m_btnArrowR : Button
-@export var m_btnMenu : Button
-@export var m_btnQuit : Button
+
 @export var m_cylinder : MeshInstance3D
 @export var m_background : WorldEnvironment
 
+@export var m_settings : Control
 
 #PRIVATE VARIABLES:
 var current = 0
@@ -59,29 +62,31 @@ func _updateCharModel(val):
 
 func _updateInfos():
 	#set the character text infos.
-	m_characterName.text = str(get_child(1).get_meta('characterName'))
-	m_artistName.text = str(get_child(1).get_meta('artistName'))
-	m_characterDescription.text = str(get_child(1).get_meta('characterDescription'))
+	_characterName.text = str(get_child(1).get_meta('characterName'))
+	_artistName.text = str(get_child(1).get_meta('artistName'))
+	_characterDescription.text = str(get_child(1).get_meta('characterDescription'))
 	
 	#set the colours.
-	m_characterName.label_settings.font_color = get_child(1).get_meta('headersColor')
-	m_artistName.label_settings.font_color = get_child(1).get_meta('headersColor')
+	_characterName.label_settings.font_color = get_child(1).get_meta('headersColor')
+	_artistName.label_settings.font_color = get_child(1).get_meta('headersColor')
 	m_background.environment.set_bg_color(get_child(1).get_meta('backgroundColor'))
 	m_cylinder.mesh.surface_get_material(0).albedo_color = get_child(1).get_meta('backgroundColor')
-	m_spt01title.label_settings.font_color = get_child(1).get_meta('headersColor')
+	_spt01title.label_settings.font_color = get_child(1).get_meta('headersColor')
 	
-	m_btnArrowR.set("theme_override_colors/icon_hover_color", get_child(1).get_meta('headersColor'))
-	m_btnArrowL.set("theme_override_colors/icon_hover_color", get_child(1).get_meta('headersColor'))
+	_stgpanel.set("theme_override_styles/bg_color", get_child(1).get_meta('backgroundColor'))
+	
+	_btnArrowR.set("theme_override_colors/icon_hover_color", get_child(1).get_meta('headersColor'))
+	_btnArrowL.set("theme_override_colors/icon_hover_color", get_child(1).get_meta('headersColor'))
 	
 	#set the sprites images.
-	m_spt01.set_texture(load(str(get_child(1).get_meta('image01'))))
-	m_spt02.set_texture(load(str(get_child(1).get_meta('image02'))))
+	_spt01.set_texture(load(str(get_child(1).get_meta('image01'))))
+	_spt02.set_texture(load(str(get_child(1).get_meta('image02'))))
 	
 	#set the sprites text infos.
-	m_spt01title.text = str(get_child(1).get_meta('img01title'))
-	m_spt01text.text = str(get_child(1).get_meta('img01text'))
-	m_spt02title.text = str(get_child(1).get_meta('img02title'))
-	m_spt02text.text = str(get_child(1).get_meta('img02text'))
+	_spt01title.text = str(get_child(1).get_meta('img01title'))
+	_spt01text.text = str(get_child(1).get_meta('img01text'))
+	_spt02title.text = str(get_child(1).get_meta('img02title'))
+	_spt02text.text = str(get_child(1).get_meta('img02text'))
 	
 	print("updated!")
 
@@ -96,4 +101,8 @@ func _on_button_quit_pressed():
 	
 #---------------------------------SETTINGS SCRIPT---------------------------------:
 
+func _on_button_menu_pressed():
+	m_settings.set_visible(true)
 
+func _on_button_quit_settings_pressed():
+	m_settings.set_visible(false)
